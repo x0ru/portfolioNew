b80df2e4f453ceac3786b9c9588fd34c27e22a24
Bart <bartlomiej.sokol123@gmail.com>
2024-07-19 16:00:17 +0100

    alpha 0.8

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,6 @@
 const scrollProgress = document.getElementById('menu');
 const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 const secondPartHeroText = document.getElementById('secondPartNameText');
-const secondPartHero = document.getElementById('secondPartName');
 
 window.addEventListener('scroll', () => {
   const scrollTop =
@@ -36,4 +35,22 @@ function changeTextHero() {
 
 changeTextHero()
 
+const menu = document.getElementById('menu');
+const menuText = document.getElementById('menuText');
+const inMenu = document.getElementById('inMenu');
+const menuOption = document.getElementById('menuOption');
+
+inMenu.addEventListener('click', openMenu);
+
+function openMenu() {
+  if (inMenu.style.scale === '0.9'){
+    menuOption.style.display='none';
+    inMenu.style.scale='1';
+    menuText.style.scale='1'
+  } else {
+    menuOption.style.display='inline-block'
+    inMenu.style.scale='0.9'
+    menuText.style.scale='1.3'
+  }
+}
 
